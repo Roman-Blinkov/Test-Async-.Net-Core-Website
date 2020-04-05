d3e212f8dcbcca257ff3479974c09c4af5af63a7
Roman Blinkov <roman@blinkov.com>
2020-04-05 21:35:02 +0300

    Add project files.

---
+++ Test Async .Net Core Website/API/InfoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Test_Async_.Net_Core_Website.API
+{
+    public class InfoModel
+    {
+        public string AppEnvironment { get; set; }
+        public string AppHost { get; set; }
+    }
+}
---
+++ Test Async .Net Core Website/Data/Test_Async_Net_Core_WebsiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Test_Async_.Net_Core_Website;
+
+namespace Test_Async_.Net_Core_Website.Data
+{
+    public class DefaultContext : DbContext
+    {
+        public DefaultContext (DbContextOptions<DefaultContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Test_Async_.Net_Core_Website.User> User { get; set; }
+    }
+}
